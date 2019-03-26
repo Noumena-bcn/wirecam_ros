c6f9537b457bd7d60f6dbd859f37ca35b9cc8d38
Starsky Lara <starsk@noumena.io>
2019-03-26 16:34:57 +0100

    Implementation PCA9685 board- test - pwm_signal test rate loop

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -13,12 +13,20 @@
 #include <time.h>
 #include <wiredbot_driver/PWMPCA9685.h>
 
-int pwm_signal = 0;
+int _pwm_signal = 0;
+int _min = 120 ;
+int _max = 720 ;
 
+int map(int x, int in_min, int in_max, int out_min, int out_max) {
+    int toReturn = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    // For debugging:
+    // printf("MAPPED %d to: %d\n", x, toReturn);
+    return toReturn;
+}
 
 void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr &msg) {
 //    ROS_INFO("Velocity-> x: [%f], y: [%f], z: [%f]", msg->linear.x, msg->linear.y, msg->linear.z);
-    pwm_signal = int(msg->linear.x);
+    _pwm_signal = map(int(msg->linear.x),1500, 1950, _min, _max);
 }
 
 int main(int argc, char **argv) {
@@ -39,10 +47,11 @@ int main(int argc, char **argv) {
         pca9685->setPWMFrequency(60);
 
         while (nh.ok()) {
-//            ROS_INFO("pwm_signal: %d", pwm_signal) ;
-            pca9685->setPWM(0,0,pwm_signal) ;
-//            ros::spinOnce();
-            ros::spin();
+            ROS_INFO("pwm_signal: %d", _pwm_signal) ;
+
+            pca9685->setPWM(0,0,_pwm_signal) ;
+            ros::spinOnce();
+//            ros::spin();
         }
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : CLOSE", pca9685->kI2CAddress);
         pca9685->closePCA9685();
