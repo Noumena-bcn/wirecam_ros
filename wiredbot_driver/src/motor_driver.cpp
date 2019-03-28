774bf26caf9edc32334af8b50531a6a47a63d9ed
Starsky Lara <starsk@noumena.io>
2019-03-28 14:53:22 +0100

    driver motor pwm config

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv) {
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : OPEN", pca9685->kI2CAddress);
         pca9685->setAllPWM(0, 0);
         pca9685->reset();
-        pca9685->setPWMFrequency(24);
+        pca9685->setPWMFrequency(50);
         int i = 0;
         while (nh.ok()) {
             if (_pwm_signal_motor != 0) {
