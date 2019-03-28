8c0b4b66acdda049f361529ada6981fa00559cad
Starsky Lara <starsk@noumena.io>
2019-03-28 15:44:18 +0100

    Implementation PCA9685 board- test - pwm_signal test rate loop

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
         while (nh.ok()) {
             if (_pwm_signal_motor != 0) {
 //                ROS_INFO("PCA9685 pwm : %d", _pwm_signal_motor);
-                ROS_INFO("PCA9685 i : %d", i);
+                ROS_INFO("PCA9685 i : %f", i);
                 pca9685->setPWM(0,0, i) ;
                 sleep(1) ;
                 i++;
