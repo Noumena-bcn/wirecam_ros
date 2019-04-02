1ed8362d793046784c5ff1374a108a3d663e33f9
Starsky Lara <starsk@noumena.io>
2019-04-02 12:45:06 +0200

    motor test

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : OPEN", pca9685->kI2CAddress);
         pca9685->setAllPWM(0, 0);
         pca9685->reset();
-        pca9685->setPWMFrequency(50);
+        pca9685->setPWMFrequency(500);
 
         uint16_t i = 0;
 
@@ -72,8 +72,8 @@ int main(int argc, char **argv) {
             if (_pwm_signal_motor >= 0 && _pwm_signal_motor <= 4095) {
                 ROS_INFO("PCA9685 pwm : %d", i);
 //                pca9685->setPWM(0, 0, i);
-                pca9685->setPWM(0, 0, 1);
-                pulseUS(i, 50);
+                pca9685->setPWM(0, 0, i);
+                pulseUS(i, 500);
                 i = i + 5;
                 sleep(1);
             } else {
