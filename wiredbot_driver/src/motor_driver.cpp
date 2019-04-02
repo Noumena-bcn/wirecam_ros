92acbfba97b27618cdc7912c6969e4eafec59d1c
Starsky Lara <starsk@noumena.io>
2019-04-02 17:16:48 +0200

    motor test

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -17,6 +17,11 @@ int MIN = 240;
 int MAX = 463;
 int _pwm_pulse = MIN;
 
+double power = 0.00;
+int pwm;
+int duty_cycle = 4095;
+
+
 // 8666 16000
 
 int _max = 1950;
@@ -69,18 +74,27 @@ int main(int argc, char **argv) {
         pca9685->setPWMFrequency(50);
 
         while (nh.ok()) {
-            for (uint16_t pwm_pulse = MIN; pwm_pulse < MAX; pwm_pulse++) {
-                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
-                pca9685->setPWM(0, 0, pwm_pulse);
-                sleep(1);
+            if (power >= 1.00)
+            {
+                ROS_INFO("PCA9685 power : %f", power);
+                pwm = -int(duty_cycle * power);
+                ROS_INFO("PCA9685 pwm : %i", pwm);
+                pca9685->setPWM(0, 0, pwm);
+                power = power + 0.01;
             }
-            sleep(1);
 
-            for (uint16_t pwm_pulse = MAX; pwm_pulse > MIN; pwm_pulse--)  {
-                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
-                pca9685->setPWM(0, 0, pwm_pulse);
-                sleep(1);
-            }
+//            for (uint16_t pwm_pulse = MIN; pwm_pulse < MAX; pwm_pulse++) {
+//                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
+//                pca9685->setPWM(0, 0, pwm_pulse);
+//                sleep(1);
+//            }
+//            sleep(1);
+//
+//            for (uint16_t pwm_pulse = MAX; pwm_pulse > MIN; pwm_pulse--)  {
+//                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
+//                pca9685->setPWM(0, 0, pwm_pulse);
+//                sleep(1);
+//            }
             ros::spinOnce();
         }
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : CLOSE", pca9685->kI2CAddress);
