071e3e02ee5198bda6fac2f2437f25b7eb008502
Starsky Lara <starsk@noumena.io>
2019-04-04 12:07:04 +0200

    motor test

--- wiredbot_driver/src/motor_driver.cpp
+++ wiredbot_driver/src/motor_driver.cpp
@@ -15,18 +15,13 @@
 
 int MIN = 240;
 int MAX = 463;
-int _pwm_pulse = MIN;
-
-double power = 0.00;
-int pwm;
-int duty_cycle = 4095;
-
+int pwm_pulse;
 
 // 8666 16000
 
-int _max = 1950;
-int servoMin = 120;
-int servoMax = 720;
+//int _max = 1950;
+//int servoMin = 120;
+//int servoMax = 720;
 
 
 int map(int x, int in_min, int in_max, int out_min, int out_max) {
@@ -38,7 +33,7 @@ int map(int x, int in_min, int in_max, int out_min, int out_max) {
 
 void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr &msg) {
 //    ROS_INFO("Velocity-> x: [%f], y: [%f], z: [%f]", msg->linear.x, msg->linear.y, msg->linear.z);
-    _pwm_pulse = msg->linear.x;
+    pwm_pulse = msg->linear.x;
 }
 
 double pulseUS(double pulse, int hz) {
@@ -71,32 +66,20 @@ int main(int argc, char **argv) {
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : OPEN", pca9685->kI2CAddress);
         pca9685->setAllPWM(0, 0);
         pca9685->reset();
-        pca9685->setPWMFrequency(50);
+        pca9685->setPWMFrequency(60);
 
         while (nh.ok()) {
-            if (power <= 1.00)
-            {
-                ROS_INFO("PCA9685 power : %f", power);
-                pwm = -int(duty_cycle * power);
-                ROS_INFO("PCA9685 pwm : %i", pwm);
-                pca9685->setPWM(0, 0, pwm);
-                power = power + 0.01;
-                sleep(1);
+            for (int i = MAX; i >= MIN; i--){
+                ROS_INFO("PCA9685 pwm : %i", MAX);
+                pca9685->setPWM(0, 0, i);
+                sleep(2);
             }
-
-//            for (uint16_t pwm_pulse = MIN; pwm_pulse < MAX; pwm_pulse++) {
-//                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
-//                pca9685->setPWM(0, 0, pwm_pulse);
-//                sleep(1);
-//            }
-//            sleep(1);
-//
-//            for (uint16_t pwm_pulse = MAX; pwm_pulse > MIN; pwm_pulse--)  {
-//                ROS_INFO("PCA9685 pwm : %i", pwm_pulse);
-//                pca9685->setPWM(0, 0, pwm_pulse);
-//                sleep(1);
-//            }
-            ros::spinOnce();
+            for (int i = MIN; i <= MAX; i++){
+                ROS_INFO("PCA9685 pwm : %i", MAX);
+                pca9685->setPWM(0, 0, i);
+                sleep(2);
+            }
+//            ros::spinOnce();
         }
         ROS_INFO("PCA9685 Device Address: 0x%02X\n : CLOSE", pca9685->kI2CAddress);
         pca9685->setPWM(0, 0, 0);
